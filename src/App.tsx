7e5f98e8923520f436274cfefbee7819dd13553f
devit <60412293+devit7@users.noreply.github.com>
2025-02-17 21:46:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl bg-blue-500 font-bold text-center mt-10">
+        Welcome to Vite + React + Tailwind CSS
+      </h1>
+      <p className="text-center mt-5">
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </>
+    );
+};
+
+export default App;
