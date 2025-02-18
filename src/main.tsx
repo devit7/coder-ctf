fcd11f8a928371759b63f27047206845cefb559b
devit <60412293+devit7@users.noreply.github.com>
2025-02-18 15:00:34 +0700

    add: unifont

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import "@fontsource/unifont/400.css";
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
