f1219b141e3e223f7902f2610e82cce721356993
devit <60412293+devit7@users.noreply.github.com>
2025-02-19 22:02:53 +0700

    update ui

--- src/pages/Challenges.tsx
+++ src/pages/Challenges.tsx
@@ -10,7 +10,10 @@ const Challenges = () => {
                     <div className="flex flex-col p-10 border-r h-full border-dashed border-zinc-700 w-[350px]">
                         <Sidebar />
                     </div>
-                    <div className=" overflow-y-auto p-10 text-gray-300 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full ">
+                    <div className=" overflow-y-auto p-10 text-gray-300 grid md:grid-cols-2 lg:grid-cols-3 h-fit  gap-4 w-full ">
+                        <ChallList />
+                        <ChallList />
+                        <ChallList />
                         <ChallList />
                     </div>
                 </div>
