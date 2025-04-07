19bcb8f85603029016ba6fd114b3d406c77b19fb
devit <60412293+devit7@users.noreply.github.com>
2025-04-07 15:11:11 +0700

    analityc

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { ThemeProvider } from "@/components/theme-provider"
 import "@fontsource/unifont/400.css";
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
+          <Analytics />
           <App />
       </BrowserRouter>
     </ThemeProvider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,18 +31,29 @@ const Home = () => {
               </span>
             </div>
             <div className="flex flex-row items-center mt-8 gap-4">
-              <button className="  bg-indigo-800 text-white px-6 py-2 rounded-md hover:bg-blue-600">
-                Get Started
-              </button>
               <a
-                href="https://discord.gg/your-discord-link"
+                href="https://discord.gg/coderctf"
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" inline-block bg-gray-800 text-white px-6 py-2 rounded-md hover:bg-gray-700"
+                className="inline-block bg-indigo-800  text-white px-6 py-2 rounded-md hover:bg-indigo-600 transition-colors"
               >
-                Join Our Discord
+                <div className="flex items-center space-x-2">
+                  <span>Join Discord</span>
+                  <svg className="w-5 h-5 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor">
+                    <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
+                  </svg>
+                </div>
               </a>
+              <button className="bg-gray-800 text-white px-6 py-2 rounded-md  hover:bg-gray-700 transition-colors">
+                Get Started
+              </button>
             </div>
+            <span className="mt-1 text-xs text-gray-400">
+              <span className="text-gray-300">Join our discord community for </span>
+              <a href="https://discord.gg/coderctf"
+                target="_blank"
+                rel="noopener noreferrer" className="text-blue-600 underline font-semibold">more information 💖</a>
+            </span>
           </div>
           <div className=" md:flex hidden justify-end items-center">
             <Code />
